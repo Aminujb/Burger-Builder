aec087d4f70d5b177373f81fd7bcb517170d2b44
Aminujb <jubrilaminu304@gmail.com>
2020-05-04 11:16:16 +0100

    Added few tests

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -7,6 +7,8 @@ import Spinner from '../../../components/UI/Spinner/Spinner'
 import Input from '../../../components/Input/Input'
 import withErrorHandler from '../../../hoc/withErrorHandler/withErrorHandler'
 import * as actions from '../../../store/actions/index'
+import {checkValidity} from '../../../shared/utility'
+
 class ContactData extends Component{
     
     state = {
@@ -116,37 +118,6 @@ class ContactData extends Component{
         
         this.props.onOrderBurger(purchaseOrder, this.props.token)
     }
-
-    checkValidity(value, rules){
-        if (!rules) {
-            return true;
-        }
-        let isValid = true
-
-        if (rules.required){
-            isValid = value.trim() !== '' && isValid
-        } 
-
-        if (rules.minLength){
-            isValid = value.length >= rules.minLength && isValid
-        }
-
-        if (rules.maxLength){
-            isValid = value.length <= rules.maxLength && isValid
-        }
-
-        if (rules.isEmail) {
-            const pattern = /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/;
-            isValid = pattern.test(value) && isValid
-        }
-
-        if (rules.isNumeric) {
-            const pattern = /^\d+$/;
-            isValid = pattern.test(value) && isValid
-        }
-
-        return isValid
-    }
     
     inputChangedHandler = (event, inputIdentifier) =>{
         const updatedOrderForm = {
@@ -156,7 +127,7 @@ class ContactData extends Component{
             ...updatedOrderForm[inputIdentifier]
         }
         updatedFormElement.value = event.target.value
-        updatedFormElement.valid = this.checkValidity(updatedFormElement.value, updatedFormElement.validation)
+        updatedFormElement.valid = checkValidity(updatedFormElement.value, updatedFormElement.validation)
         updatedFormElement.touched = true
         updatedOrderForm[inputIdentifier] = updatedFormElement
         
